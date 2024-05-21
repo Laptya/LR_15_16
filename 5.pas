df6faa561ea118be90c5557f290cdd9916538e3d
Laptya <144488450+Laptya@users.noreply.github.com>
2024-05-21 12:42:20 +0300

    Update 5.pas

--- 5.pas
+++ 5.pas
@@ -1,22 +1,19 @@
-//В данном коде используется статическая структура стека. 
-//Основная особенность стека заключается в том, что данные 
-//хранятся в порядке последним пришел - первым вышел (Last In, First Out - LIFO).
 
 const max = 100;
-//Состав узла структуры Stack:
+
 type Stack = record  
-      tags: array[1..max] of integer; //массив для хранения элементов стека
-      size: integer; //переменная для хранения текущего размера стека
+      tags: array[1..max] of integer;
+      size: integer; 
      end;
 
-procedure Push( var S: Stack; x: integer);//процедура для добавления элемента в стек
+procedure Push( var S: Stack; x: integer);
 begin
   if S.size = max then Exit;  
   S.size := S.size + 1;
   S.tags[S.size] := x; 
 end;
 
-function Pop ( var S:Stack ): integer;//функция для извлечения элемента из стека
+function Pop ( var S:Stack ): integer;
 begin
   if S.size = 0 then begin
     Result := 0;
@@ -26,7 +23,7 @@ begin
   S.size := S.size - 1;
 end;
 
-function isEmptyStack ( S: Stack ): Boolean;//функция для проверки стека на пустоту
+function isEmptyStack ( S: Stack ): Boolean;
 begin
   Result := (S.size = 0);
 end;
